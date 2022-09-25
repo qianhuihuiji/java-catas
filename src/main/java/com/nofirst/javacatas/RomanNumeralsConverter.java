c8a8edfca06ba698a35c757fade75c901075e8c5
qianhuihuiji <1025926969@qq.com>
2022-09-25 13:32:10 +0800

    PrimeFactors test:refactory

--- src/main/java/com/nofirst/javacatas/RomanNumeralsConverter.java
+++ src/main/java/com/nofirst/javacatas/RomanNumeralsConverter.java
@@ -1,31 +1,52 @@
 package com.nofirst.javacatas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RomanNumeralsConverter {
+
+    private static final List<RomanNumeralMap> lookup = new ArrayList<>();
+
+    static {
+        lookup.add(new RomanNumeralMap(5, "V"));
+        lookup.add(new RomanNumeralMap(4, "IV"));
+        lookup.add(new RomanNumeralMap(1, "I"));
+    }
 
     public String convert(int number) {
         StringBuilder solution = new StringBuilder();
 
-        while(number >= 5)
-        {
-            solution.append("V");
+        for (RomanNumeralMap entry : lookup) {
+            Integer limit = entry.getKey();
+            String glyph = entry.getValue();
 
-            number -= 5;
-        }
+            while(number >= limit)
+            {
+                solution.append(glyph);
 
-        while(number >= 4)
-        {
-            solution.append("IV");
-
-            number -= 4;
-        }
-
-        while(number >= 1)
-        {
-            solution.append("I");
-
-            number -= 1;
+                number -= limit;
+            }
         }
 
         return solution.toString();
+    }
+
+    static class RomanNumeralMap {
+
+        private final Integer key;
+        private final String value;
+
+        public Integer getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public RomanNumeralMap(Integer key, String value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }

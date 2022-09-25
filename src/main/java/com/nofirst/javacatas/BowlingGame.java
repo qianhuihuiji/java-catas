7816e8d4fe2784d598320e19cf313d648116b669
qianhuihuiji <1025926969@qq.com>
2022-09-25 20:27:37 +0800

    BowlingGameTest:refactory

--- src/main/java/com/nofirst/javacatas/BowlingGame.java
+++ src/main/java/com/nofirst/javacatas/BowlingGame.java
@@ -1,21 +1,24 @@
 package com.nofirst.javacatas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-    private int score = 0;
+    private List<Integer> rolls = new ArrayList<>();
 
     /**
      *
      * 数表示被击倒的瓶子数，如 roll(0) 表示击倒了 0 个瓶子
      *
-     * @param roll 击倒的瓶子数
+     * @param pints 击倒的瓶子数
      */
-    public void roll(int roll)
+    public void roll(int pints)
     {
-        this.score += roll;
+        this.rolls.add(pints);
     }
 
     public int score() {
-        return this.score;
+        return this.rolls.stream().reduce(Integer::sum).orElse(0);
     }
 }

40d73c4536679ac07bb8004c98b6f27938b4e1b7
qianhuihuiji <1025926969@qq.com>
2022-09-25 10:51:24 +0800

    add java-catas

---
+++ src/main/java/com/nofirst/javacatas/JavaCatasApplication.java
@@ -0,0 +1,13 @@
+package com.nofirst.javacatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nofirst/javacatas/JavaCatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nofirst.javacatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

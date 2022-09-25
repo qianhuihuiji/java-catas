375d99d86b6af4b81514b61c792109cc4df782f1
qianhuihuiji <1025926969@qq.com>
2022-09-25 11:09:20 +0800

    PrimeFactors test:it_returns_2_for_2

--- src/main/java/com/nofirst/javacatas/PrimeFactors.java
+++ src/main/java/com/nofirst/javacatas/PrimeFactors.java
@@ -2,6 +2,10 @@ package com.nofirst.javacatas;
 
 public class PrimeFactors {
     public int[] generate(int i) {
+        if (i > 1) {
+            return new int[]{2};
+        }
+
         return new int[]{};
     }
 }
--- src/test/java/com/nofirst/javacatas/PrimeFactorsTest.java
+++ src/test/java/com/nofirst/javacatas/PrimeFactorsTest.java
@@ -7,11 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeFactorsTest {
 
+    private final PrimeFactors primeFactors = new PrimeFactors();
+
     @Test
     public void it_returns_an_empty_array_for_1()
     {
-        PrimeFactors primeFactors = new PrimeFactors();
-        assertThat(primeFactors.generate(1)).isEmpty();
+        assertThat(this.primeFactors.generate(1)).isEmpty();
     }
 
+    @Test
+    public void it_returns_2_for_2()
+    {
+        assertThat(this.primeFactors.generate(2)).isEqualTo(new int[]{2});
+    }
 }
